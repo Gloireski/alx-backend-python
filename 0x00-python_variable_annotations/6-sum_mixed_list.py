b551caa5a4989dd413b0683f0da4ce35506504ee
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-04 18:34:10 +0000

    task 6 fixed

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import List, Union
 """
 define sum_mixed_list
 """
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
