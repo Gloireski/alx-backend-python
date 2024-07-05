7ea214b8d8fd8fd92b88afa3d9d24782f8a63be3
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-05 00:10:31 +0000

    task 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """
+ducking type
 """
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
+    The types of the elements of the input are not know
     """
     if lst:
         return lst[0]
