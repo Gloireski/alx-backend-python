1928726dec39128da73769253cab2e9539b078f2
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-08 13:34:28 +0000

    init et t0

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# 0-basic_async_syntax.py
+import random
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    """ asynchronous coroutine """
+    r = random.uniform(0, max_delay)
+    return r
