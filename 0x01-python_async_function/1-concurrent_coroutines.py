f118a21b66361b9a378d0b894d7a18495d9271a8
Gloireski <belemgloirebekoutou8@gmail.com>
2024-07-08 21:26:11 +0000

    task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" define fn execute multiple caroutines """
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ return the list of all the delays (float values). """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = asyncio.as_completed(tasks)
+    delays = [await task for task in tasks]
+    return delays
